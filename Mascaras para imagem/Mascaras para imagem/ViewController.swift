cb77f0059e06891e5113e914e2832fac9df7d85e
Gabriel Sanzone <gabriel.sanzone@fcamara.com.br>
2019-08-01 09:25:34 -0300

    0.0.0.0
    https://www.youtube.com/watch?v=xfrrZfbxFb0

---
+++ Mascaras para imagem/Mascaras para imagem/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Mascaras para imagem
+//
+//  Created by Gabriel Chirico Mahtuk de Melo Sanzone on 01/08/19.
+//  Copyright © 2019 Gabriel Chirico Mahtuk de Melo Sanzone. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var imagemPerfil: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        let mask = UIImageView
+        imagemPerfil.mask
+    }
+
+
+}
+
